4d05ca1ff3cc6d2488c3a20e2c1af4f9615fd5c8
Hisham C <hishamc18@gmail.com>
2024-11-13 14:34:36 +0530

    nov 13

--- practice.js
+++ practice.js
@@ -2017,3 +2017,19 @@
 //     }
 //     return val;
 // };
+
+
+
+
+// // 35. Search Insert Position
+// var searchInsert = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] >= target) {
+//             return i;
+//         }
+//     }
+//     return nums.length;
+// };
+// console.log(searchInsert([1,3,5,6],5));
+// console.log(searchInsert([1,3,5,6],2));
+// console.log(searchInsert([1,3,5,6],7)); 
