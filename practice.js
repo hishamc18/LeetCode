931602ebe6e78cec939d1e22be034cc46d725b5a
Hisham C <hishamc18@gmail.com>
2024-11-21 09:40:28 +0530

    nov 21

--- practice.js
+++ practice.js
@@ -2165,3 +2165,28 @@ const { STRING } = require("mysql/lib/protocol/constants/types");
 //     return score
 // };
 // console.log(scoreOfString('hello'));
+
+
+
+
+
+// // 3069. Distribute Elements Into Two Arrays I
+// var resultArray = function(nums) {
+//     let arr1 = [];
+//     let arr2 = [];
+
+//     arr1.push(nums[0]);
+//     if(nums.length > 1){
+//         arr2.push(nums[1])
+//     }
+
+//     for(let i = 2; i<nums.length;i++){
+//         if(arr1[arr1.length-1] > arr2[arr2.length-1]){
+//             arr1.push(nums[i])
+//         }else{
+//             arr2.push(nums[i])
+//         }
+//     }
+//     return [...arr1,...arr2]
+// };
+// console.log(resultArray([2,1,3]));
