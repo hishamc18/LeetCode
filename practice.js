e55e50ad2dde3356ee12ffda9651f91c8eb4d8e3
Hisham C <hishamc18@gmail.com>
2024-12-23 14:42:19 +0530

    leetcode dec 23

--- practice.js
+++ practice.js
@@ -2402,3 +2402,23 @@
 
 
 
+// // 11. Container With Most Water
+// var maxArea = function(height) {
+//     let left = 0
+//     let right = height.length - 1
+//     let max = 0
+
+//     while (left < right){
+//         var area = Math.min(height[left], height[right]) * (right-left)
+//         max = Math.max(max, area)
+
+//         if(height[left] < height[right]){
+//             left++
+//         } else {
+//             right --
+//         }
+//     }
+//     return max
+// };
+// console.log(maxArea([1,8,6,2,5,4,8,3,7]));
+// console.log(maxArea([1,1]));
