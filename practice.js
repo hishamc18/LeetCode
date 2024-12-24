41a3b9a62bc99abae823be67f76d55189c3ce8eb
Hisham C <hishamc18@gmail.com>
2024-12-24 14:39:05 +0530

    leetcode dec 24

--- practice.js
+++ practice.js
@@ -2422,3 +2422,42 @@
 // };
 // console.log(maxArea([1,8,6,2,5,4,8,3,7]));
 // console.log(maxArea([1,1]));
+
+
+
+// //customMap of map method
+// const customMap = (arr, callback) => {
+//     let result = []
+//     for(let i = 0; i<arr.length; i++){
+//         result.push(callback(arr[i], i, arr))
+//     }
+//     return result
+// };
+// let a = [1,2,3];
+// let newResult = customMap(a, (num) => num*10)
+// console.log(newResult);
+
+
+
+// // 704. Binary Search
+// var search = function(nums, target) {
+//     let left = 0;
+//     let right = nums.length - 1
+
+//     while(left <= right){
+//         let mid = Math.floor((left + right)/2)
+
+//         if(nums[mid] == target){
+//             return mid
+//         }
+//         else if (nums[mid] > target){
+//             right = mid - 1
+//         }
+//         else {
+//             left = mid + 1
+//         }
+//     }
+//     return -1
+// };
+// console.log(search([-1,0,3,5,9,12],9));
+
