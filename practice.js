a74f09503e4f4fd6089daf341330bd99862fd550
Hisham C <hishamc18@gmail.com>
2025-01-10 09:40:18 +0530

    leetcode jan 10

--- practice.js
+++ practice.js
@@ -2593,3 +2593,19 @@ const { DATE } = require("mysql/lib/protocol/constants/types")
 //         return note
 //     }
 // };
+
+
+
+// 2185. Counting Words With a Given Prefix
+// var prefixCount = function(words, pref) {
+//     let count = 0
+
+//     for(let i = 0; i<words.length; i++){
+//         if(words[i].startsWith(pref)){
+//             count++
+//         }
+//     }
+//     return count
+// };
+// console.log(prefixCount(["pay","attention","practice","attend"], 'at'));
+
