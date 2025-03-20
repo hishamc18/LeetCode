9f2e9d8be1402daa0ae4b3e70a68306109b8e5ee
Hisham C <hishamc18@gmail.com>
2025-03-20 10:31:44 +0530

    Time: 3 ms (35.86%), Space: 56.3 MB (78.49%) - LeetHub

--- 3778-transform-array-by-parity/3778-transform-array-by-parity.js
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.js
@@ -3,10 +3,5 @@
  * @return {number[]}
  */
 var transformArray = function(nums) {
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] % 2 === 0 ){
-            nums[i] = 0
-        } else nums[i] = 1
-    }
-    return nums.sort((a,b)=>a-b)
+    return nums.map(item => item % 2 === 0 ? 0 : 1).sort((a, b) => a - b)
 };
