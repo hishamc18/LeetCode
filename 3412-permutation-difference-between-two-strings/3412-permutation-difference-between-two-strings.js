ed8a9b426ed940936fc97d0e432a43d26ca3fb7f
Hisham C <hishamc18@gmail.com>
2025-03-21 10:15:34 +0530

    Time: 0 ms (100%), Space: 55.2 MB (43.27%) - LeetHub

---
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var findPermutationDifference = function(s, t) {
+    let res = 0;
+    for (let i = 0; i < s.length; i++) {
+        res += Math.abs(i - t.indexOf(s[i]));
+    }
+    return res;
+};
+console.log(findPermutationDifference("abc","bac"));
