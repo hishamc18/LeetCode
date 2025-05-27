6fc88ebc0b11712a7253a5a788becaee9287c5aa
Hisham C <hishamc18@gmail.com>
2025-05-27 17:57:54 +0530

    Time: 8 ms (22.28%), Space: 59.5 MB (12.44%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+      const result = [];
+
+  for (let i = left; i <= right; i++) {
+    const digits = i.toString().split('');
+    if (digits.every(d => d !== '0' && i % Number(d) === 0)) {
+      result.push(i);
+    }
+  }
+
+  return result;
+};
