c79e30181618ba67778640f260b2795ae804aed4
Hisham C <hishamc18@gmail.com>
2025-05-27 18:23:48 +0530

    Time: 0 ms (100%), Space: 54.2 MB (43.94%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    if(nums.includes(target)){
+        return true
+    }else{
+        return false
+    }
+};
