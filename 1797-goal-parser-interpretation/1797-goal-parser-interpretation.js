6bec5a33a98486e21d2226e78834da7e3f527689
Hisham C <hishamc18@gmail.com>
2025-06-18 12:44:10 +0530

    Time: 44 ms (44.2%), Space: 54.7 MB (16.96%) - LeetHub

---
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function(command) {
+    let res = "";
+    for (let i = 0; i < command.length; i++) {
+        if (command[i] === 'G') {
+            res += 'G';
+        } else if (command[i] === '(' && command[i + 1] === ')') {
+            res += 'o';
+        } else if (command[i] === '(' && command.slice(i, i + 4) === '(al)') {
+            res += 'al';
+        }
+    }
+    return res;
+};
